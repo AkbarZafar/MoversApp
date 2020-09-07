ec512a3d93c967a5bfc393497cd8211550525615
Akbar Zafar <jaheeko@gmail.com>
2020-09-07 16:28:20 -0400

    removed files

--- backEnd/Service.py
+++
@@ -1,8 +0,0 @@
-from models import BoxesModel
-
-class BoxesService:
-  def __init__(self):
-    self.model = BoxesModel()
-
-  def create(self, params):
-    self.model.create(params["text"], params["Description"], params["UserId"])
