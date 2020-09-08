c63d81cdf5cc6c0a2401f85ad0389b328df1f0d8
Akbar Zafar <jaheeko@gmail.com>
2020-09-08 14:18:24 -0400

    setting up jwt authorization

--- backEnd/app.py
+++ backEnd/app.py
@@ -1,87 +1,76 @@
+import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String, ForeignKey
-import os
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_current_user
+
 
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
   os.path.join(basedir, 'Movers.db')
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
-
-def db_create():
-  db.create_all()
-  print('DB CREATED')
-
-
-# @app.cli_command('db_drop')
-# def db_drop():
-#   db.drop_all
-#   print('DB DROPPED')
-
-
-def db_seed():
-  user1 = User(
-    username='User1',
-    email='email@email.com',
-    password='p@ss'
-  )
-  user2 = User(
-    username='User2',
-    email='email2@email2.com',
-    password='p@ssw0rd'
-  )
-
-  living_room = Boxes(
-    name='living_room',
-    user=user1,
-  )
-
-  dining_room = Boxes(
-    name='dining_room',
-    user=user1
-  )
-
-  pillow = Items(
-    name='pillow',
-    box_id=1
-  )
-
-  bedsheets = Items(
-    name='bedsheets',
-    box_id=1
-  )
-
-  db.session.add(user1)
-  db.session.add(user2)
-  db.session.add(living_room)
-  db.session.add(dining_room)
-  db.session.add(pillow)
-  db.session.add(bedsheets)
-
-  db.session.commit()
-  print('DB SEEDED')
 
 @app.route('/')
 def home():
   return jsonify(message='hello!')
 
 
-@app.route('/box')
+@app.route('/box', methods=['GET'])
+@jwt_required
 def get_box_list():
-  userID = int(request.args.get('userID'))
-  box = request.args.get('box')
+  current_user = get_current_user()
 
-  if box:
-    # get a single box
-    return 1
+  return jsonify(message=current_user)
+
+  # if box:
+  #   # get a single box
+  #   return 1
+  # else:
+  #   return 2
+  #   # get all the the boxes by that user.
+
+
+# @app.route('/box', methods=['POST'])
+# def 
+
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+  email = request.json['email']
+
+  test = User.query.filter_by(email=email).first()
+  if(test):
+    return jsonify(message='That email already exists')
   else:
-    return 2
-    # get all the the boxes by that user.
+    username = request.json['username']
+    password = request.json['password']
+    user = User(username=username, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(message="User created sucessfully")
+
+
+@app.route('/login', methods=['GET'])
+def login():
+  email = request.json['email']
+  password = request.json['password']
+
+  test = User.query.filter_by(email=email, password=password).first()
+  if test:
+    access_token = create_access_token(identity=email)
+    return jsonify(message= 'login suceeded', access_token=access_token)
+  else:
+    return jsonify(message='wrong credentials')
+
+
 
 
 # database models
@@ -97,7 +86,7 @@ class User(db.Model):
 class Boxes(db.Model):
   __tablename__ = 'Boxes'
 
-  id = Column(Integer, primart_key=True)
+  id = Column(Integer, primary_key=True)
   name = Column(String)
   user = Column(ForeignKey('Users.id'))
   items = relationship('Items')
@@ -108,6 +97,11 @@ class Items(db.Model):
   name = Column(String)
   box_id = Column(Integer, ForeignKey('Boxes.id'))
 
+# database creation
+@app.route('/db_create')
+def create_db():
+  db.create_all()
+  return 'database created'
 
 if __name__ == "__main__":
   app.run(debug=True)
